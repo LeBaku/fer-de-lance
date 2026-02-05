35d88d5436d2e186f09c0323c03c1ca7e6b1fe5c
Baku <jules.magyari@epitech.eu>
2026-02-05 15:49:37 +0100

    fix a little mistake hihihi

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Provider } from './components/ui/provider'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-    ²<App />
+      <App />
     </Provider>
   </StrictMode>,
 )
