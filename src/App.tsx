b559b577b5f72d63fad1c8c44046f9242cccf3c2
Baku <jules.magyari@epitech.eu>
2026-02-05 16:57:25 +0100

    Add more informations on every units

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { supabase } from './lib/supabaseClient'
-import { Container, Heading, Text, VStack, Box, Image } from '@chakra-ui/react'
+import { Container, Heading, Text, VStack, Box, Image, Flex } from '@chakra-ui/react'
 
 function App() {
   const [data, setData] = useState<any[]>([])
@@ -8,12 +8,19 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
+      const cached = localStorage.getItem('units-cache')
+      if (cached) {
+        setData(JSON.parse(cached))
+        setLoading(false)
+      }
+
       const { data: units, error } = await supabase.from('units').select('*')
 
       if (error) {
         console.error('Erreur:', error)
       } else {
-        setData(units || [])
+        setData(units)
+        localStorage.setItem('units-cache', JSON.stringify(units))
       }
       setLoading(false)
     }
@@ -27,17 +34,28 @@ function App() {
         <Heading>Mes Unités Spearhead</Heading>
         {loading ? (
           <Text>Chargement des renforts...</Text>
-        ) : data.length > 0 ? (
-          data.map((item) => (
-            <Box key={item.id} p={5} shadow="md" borderWidth="1px" borderRadius="md" bg="gray.300">
-              <Heading size="md">{item.name}</Heading>
-              <Text mt={2}>{item.size}</Text>
+        ) : data.map((item) => (
+          <Box key={item.id} p={0} overflow="hidden" borderWidth="1px" borderRadius="xl" bg="gray.800">
+            <Flex direction={{ base: "column", md: "row"}}>
               <Image src={item.image_url} objectFit="cover" boxSize={{ base: "100%", md: "200px" }} />
-            </Box>
-          ))
-        ) : (
-          <Text>Aucune donnée trouvée. Vérifie ta table Supabase !</Text>
-        )}
+              <VStack align="start" p={5} gap={2}>
+                <Flex justify="space-between" width="100%" align="center">
+                  <Heading size="md" color="white">{item.name}</Heading>
+                </Flex>
+                <Box>
+                  <Text fontSize="xs" fontWeight="bold" color="gray.500" mb={1}>
+                    MOT CLES
+                  </Text>
+                  <Text fontSize="sm" color="gray.300" fontStyle="italic">
+                    {Array.isArray(item.keywords)
+                      ? item.keywords.join(", ")
+                      : item.keywords?.split(',').map((k: string) => k.trim()).join(', ')}
+                  </Text>
+                </Box>
+              </VStack>
+            </Flex>
+          </Box>
+        ))}
       </VStack>
     </Container>
   )
